aebb3ead358332ccff4654e1a4b39688a418f41a
JunaidbhaiAk <49246469+JunaidbhaiAk@users.noreply.github.com>
2023-09-02 23:34:39 +0530

    Some fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/Root.tsx";
@@ -9,10 +8,9 @@ import ModalProvider from "./context/modal.context.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import Analysis from "./routes/Analysis.tsx";
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -32,13 +30,11 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
     <ModalProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </ModalProvider>
-    </Provider>
-  </React.StrictMode>
+  </Provider>
 );
